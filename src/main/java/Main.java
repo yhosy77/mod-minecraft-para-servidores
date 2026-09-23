3ec99ebf980c52f77903512a9724a62fbd4a0c5f
DAVID SANTIAGO DIAZ LOZANO <d202514822@gmail.com>
2026-09-22 21:45:18 -0500

    Inicio del código
    Nada, solo hice un "PRINT"

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Aquí empieza la gran historia gente...");
+        System.out.println("Hello World :D");
+    }
+}
